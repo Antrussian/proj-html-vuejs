9f68658c7acaaa85f89579162a4fc4ca1d0f2b50
Antrussian <antonino.russo.agent@gmail.com>
2024-01-10 10:55:45 +0100

    Componente Header + Recupero metodologià di lavoro

--- gym/src/App.vue
+++ gym/src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Header :menuItems="menuItems" />
+ 
+    <!-- Aggiungi altri componenti se necessario -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+import Header from './components/Header.vue';
+
+
+export default {
+  components: {
+    Header,
+
+  },
+  data() {
+    return {
+      menuItems: [
+        { id: 1, label: 'HOME' },
+        { id: 2, label: 'ABOUT US' },
+        { id: 3, label: 'FACILITIES' },
+        { id: 4, label: 'MEMBERSHIP' },
+        { id: 5, label: 'TESTIMONIALS' },
+        { id: 6, label: 'BLOG' },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* Aggiungi stili globali qui se necessario */
 </style>
---
+++ gym/src/components/Header.vue
@@ -0,0 +1,96 @@
+<template>
+  <header class="header-container">
+    <!-- Immagine di sfondo e logo -->
+<div class="top-bar">
+      <div class="logo-container">
+      <img src="../assets/img/gym_logo_2x.png" alt="Logo" class="img-fluid logo-header">
+    </div>
+
+    <!-- Menu con voci popolate dinamicamente -->
+    <nav>
+      <ul class="navbar-nav">
+        <li v-for="item in menuItems" :key="item.id" class="nav-item">
+          <a class="nav-link" href="#">{{ item.label }}</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+
+    <div class="header-content">
+      <h1 class="text-center">NO PAIN NO GAIN</h1>
+      <button class="btn btn-primary">JOIN US TODAY</button>
+    </div>
+
+
+
+
+
+
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    menuItems: Array,
+  },
+};
+</script>
+
+<style>
+/* Aggiungi stili specifici per l'header */
+
+.logo-container {
+width: 20%;
+}
+.header-container {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-between;
+  padding: 20px;
+  background-color: #333; /* Aggiungi il colore di sfondo desiderato */
+  color: white;
+  width: 100%;
+  height: 600px;
+}
+
+.header-background {
+  height: 600px;
+  width: 100%;
+}
+
+.logo-header {
+  width: 100%;
+}
+
+.top-bar{
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+
+.navbar-nav {
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.nav-item {
+  margin-right: 15px;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: white;
+}
+
+.header-content {
+  text-align: center;
+}
+
+</style>
