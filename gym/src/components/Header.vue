b10d1b2ed67599610e238b7939eab37d803345af
Antrussian <antonino.russo.agent@gmail.com>
2024-01-10 14:38:34 +0100

    Components header finito

--- gym/src/components/Header.vue
+++ gym/src/components/Header.vue
@@ -1,10 +1,9 @@
 <template>
   <header class="header-container">
-    <!-- Immagine di sfondo e logo -->
+
 <div class="top-bar">
-      <div class="logo-container">
-      <img src="../assets/img/gym_logo_2x.png" alt="Logo" class="img-fluid logo-header">
-    </div>
+      
+      <img src="../assets/images/gym_logo_1x.png" alt="Logo" class="img-fluid logo-header">
 
     <!-- Menu con voci popolate dinamicamente -->
     <nav>
@@ -19,7 +18,8 @@
 
     <div class="header-content">
       <h1 class="text-center">NO PAIN NO GAIN</h1>
-      <button class="btn btn-primary">JOIN US TODAY</button>
+      <p class="sub-title">Motivation is what gets you started. habit is what keeps you going.</p>
+      <button class="btn btn-primary cta-header-button">JOIN US TODAY</button>
     </div>
 
 
@@ -39,58 +39,87 @@ export default {
 </script>
 
 <style>
-/* Aggiungi stili specifici per l'header */
 
-.logo-container {
-width: 20%;
-}
+
+
 .header-container {
   display: flex;
   flex-direction: column;
-
+background-image: url(../assets/images/home-banner.jpg);
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
   justify-content: space-between;
   padding: 20px;
-  background-color: #333; /* Aggiungi il colore di sfondo desiderato */
+  background-color: #333; 
   color: white;
-  width: 100%;
-  height: 600px;
+  
 }
 
-.header-background {
-  height: 600px;
-  width: 100%;
-}
 
-.logo-header {
-  width: 100%;
-}
+
+
 
 .top-bar{
   display: flex;
   flex-direction: row;
   width: 100%;
+  justify-content: space-between;
 }
 
 .navbar-nav {
   display: flex;
   flex-direction: row;
-  width: 80%;
+width: 60%;
   list-style: none;
   margin: 0;
   padding: 0;
+  flex-wrap: nowrap;
 }
 
 .nav-item {
   margin-right: 15px;
+
+
+
 }
 
+.logo-header {
+  width: 200px;
+}
 .nav-link {
   text-decoration: none;
   color: white;
+ font-size: 10px;
+ width: 100%;
+ white-space: nowrap;
+ 
+}
+
+.nav-link:hover {
+  text-decoration: none;
+  color: rgb(180, 15, 15);
+ font-size: 10px;
+ width: 100%;
+ white-space: nowrap;
+ 
 }
 
 .header-content {
   text-align: center;
+  padding: 10rem;
+  font-size: 30px;
 }
 
+.sub-title {
+  font-size: 12px;
+  margin-bottom: 30px;
+}
+.cta-header-button{
+  background-color: rgba(240, 248, 255, 0);
+  padding: 15px;
+  color: rgba(255, 255, 255, 0.489);
+  border-radius: 5px;
+  border-color: rgba(255, 255, 255, 0.264);
+}
 </style>
